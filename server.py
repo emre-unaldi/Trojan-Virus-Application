cb32de25f2befac0b4cff06e8cc705e72c0f34b1
emre-unaldi <emree.unaldi@gmail.com>
2022-11-09 01:03:12 +0300

    Messaging app to understand backdoor logic

--- server.py
+++ server.py
@@ -9,9 +9,24 @@ socket_connection.bind(("192.168.1.106", 7171))
 socket_connection.listen(1)
 
 print("Hedef sistemin bağlantı kurması bekleniyor...")
-kurban, ip=socket_connection.accept()
-
+victim_person, ip=socket_connection.accept()
 print("Hedef sistem ile bağlantı sağlandı.")
 print("Hedef sistem bilgileri : %s" % str(ip))
+print("[0] ==> Programı Sonlandır")
+
+while True:
+    message = input("Mesajınızı yazın : ")
+    encode_message = message.encode()
+    
+    if message == "0":
+        victim_person.send(encode_message)
+        print("Sunucu Kapatıldı.")
+        break
+
+    else:
+        victim_person.send(encode_message)
+        response = victim_person.recv(4096)
+        decode_message = response.decode()
+        print("Hedef sistem cevabı : " + decode_message)
 
 socket_connection.close()
--- trojan.py
+++ trojan.py
@@ -1,8 +1,23 @@
 #! /usr/bin/env python
 # encoding:utf-8
+
 import socket
 
-socket_connection=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_connection.connect(("192.168.1.106", 7171))
+
+while True:
+    message = socket_connection.recv(4096)
+    decode_message = message.decode()
+    print("Sunucu mesajı : " + decode_message)
+
+    if decode_message == "0":
+        print("Sunucu uygulamayı sonlandırdı.")
+        break
+        
+    else:
+        response = input("Cevabınızı yazın : ")
+        encode_message = response.encode()
+        socket_connection.send(encode_message)
 
 socket_connection.close()
